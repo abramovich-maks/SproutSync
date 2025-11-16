992e012fdd3ce2dd4fe0ad428ec643beadb1cd46
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-11-16 23:59:54 +0100

    Created `BaseEntity` with common fields and mappings for entity inheritance.

---
+++ src/main/java/com/sproutsync/domain/util/util/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.sproutsync.domain.util.util;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    public UUID uuid = UUID.randomUUID();
+
+    @CreationTimestamp
+    public Instant createdOn;
+
+    @Version
+    private long version;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(uuid, that.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+}
