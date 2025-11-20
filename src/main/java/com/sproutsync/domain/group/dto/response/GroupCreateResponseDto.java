563c076a7e5fbd918bf884c6c046e2f7cb7c2598
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-11-20 16:22:50 +0100

    Refactored `GroupCreateResponseDto` to use `record` for improved immutability and readability.

--- src/main/java/com/sproutsync/domain/group/dto/response/GroupCreateResponseDto.java
+++ src/main/java/com/sproutsync/domain/group/dto/response/GroupCreateResponseDto.java
@@ -7,17 +7,18 @@ import javax.validation.constraints.NotBlank;
 
 @Builder
 @Schema(name = "GroupResponse", description = "Response DTO representing a group")
-public class GroupCreateResponseDto {
+public record GroupCreateResponseDto(
 
-    @Schema(description = "Unique ID of the group", example = "12")
-    private Long id;
+        @Schema(description = "Unique ID of the group", example = "12")
+        Long id,
 
-    @NotBlank
-    @Schema(description = "Name of the group", example = "Biedronki")
-    private String name;
+        @NotBlank
+        @Schema(description = "Name of the group", example = "Biedronki")
 
-    @NotBlank
-    @Schema(description = "Short description of the group", example = "This is the preschool group for 3-4 year olds")
-    private String description;
+        String name,
 
+        @NotBlank
+        @Schema(description = "Short description of the group", example = "This is the preschool group for 3-4 year olds")
+        String description
+) {
 }
